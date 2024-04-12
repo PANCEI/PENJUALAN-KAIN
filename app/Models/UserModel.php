0cc40feedb2e84af9512e18b96e901f151e5c010
Pance <aswarhamid12@gmail.com>
2024-04-13 01:53:11 +0800

    setting login

---
+++ app/Models/UserModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table            = 'user';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [];
+
+    protected bool $allowEmptyInserts = false;
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'DATE';
+    protected $createdField  = 'tanggal_update';
+    
+
+    public function getuser($email){
+    
+    }
+}
